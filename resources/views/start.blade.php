e9a794faf6a898355e8df56d5706116bec578b4a
Angelica-92 <Angelica_92-@hotmail.com>
2017-05-22 14:38:49 +0200

    Vyernas grunder är klara

---
+++ resources/views/start.blade.php
@@ -0,0 +1,37 @@
+
+@extends('products.master')
+
+@section('content')
+
+<div class="row">
+  <div class="col-md-4">
+  </div>
+  <div class="col-md-4">
+    <h2>Mobiler</h2>
+  </div>
+  <div class="col-md-4">
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+  </div>
+  <div class="col-md-4">
+    <h2>Surfplattor</h2>
+  </div>
+  <div class="col-md-4">
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-4">
+  </div>
+  <div class="col-md-4">
+    <h2>Datorer</h2>
+  </div>
+  <div class="col-md-4">
+  </div>
+</div>
+
+
+@endsection

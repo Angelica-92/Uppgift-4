94215fbf42df33691f9da97cc26791006b068ca7
Angelica-92 <Angelica_92-@hotmail.com>
2017-05-22 12:09:59 +0200

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+//Requires user to be logged in to access pages
+Route::group(["middleware" => "auth"], function () {
+  Route::resource('stores', 'StoresController', ['except' => [
+      'show', 'index', 'start'
+  ]]);
+  Route::resource('reviews', 'ReviewsController', ['except' => [
+      'show', 'index', 'start'
+  ]]);
+  Route::resource('mobiles', 'MobilesController', ['except' => [
+      'show', 'index', 'start'
+  ]]);
+  Route::resource('tablets', 'TabletsController', ['except' => [
+      'show', 'index', 'start'
+  ]]);
+  Route::resource('computers', 'ComputerssController', ['except' => [
+      'show', 'index', 'start'
+  ]]);
+});
+
+//index, show and start can be accessed without being logged in
+Route::resource('reviews', 'ReviewsController', ['only' => [
+    'index', 'show', 'start'
+]]);
+Route::resource('stores', 'StoresController', ['only' => [
+    'index', 'show', 'start'
+]]);
+Route::resource('mobiles', 'MobilesController', ['only' => [
+    'index', 'show', 'start'
+]]);
+Route::resource('tablets', 'TabletsController', ['only' => [
+    'index', 'show', 'start'
+]]);
+Route::resource('computers', 'ComputersController', ['only' => [
+    'index', 'show', 'start'
+]]);

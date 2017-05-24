e5501c71b8f1e157e414a5006f8f25519795d2e6
Angelica-92 <Angelica_92-@hotmail.com>
2017-05-24 14:48:02 +0200

    små fix med start

--- resources/views/mobiles/show.blade.php
+++ resources/views/mobiles/show.blade.php
@@ -36,38 +36,6 @@
     <div class="col-md-2">
     </div>
     <div class="col-md-8">
-      <form action="/mobiles" method="post">
-        {{ csrf_field() }}
-      <div class="form-group">
-        <label for="title">Namn</label>
-        <input type="text" class="form-control" id="name" type="mobile" name="name" placeholder="Skriv namn här...">
-    </div>
-    <div class="form-group">
-    <label for="comment">Kommentar:</label>
-    <textarea class="form-control" rows="5" id="comment" type="mobile" name="comment"></textarea>
-  </div>
-  <div class="form-group">
-  <label for="rating">Välj ett betyg:</label>
-  <select type="number" class="form-control" id="rating" type="mobile" name="rating">
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
-    <option>5</option>
-  </select>
-  </div>
-  <input type="hidden" class="form-control" type="mobile" id="{{ $mobile->id }}" name="{{ $mobile->id }}" value="{{ $mobile->id }}" placeholder="Add the id here..." required>
-    <input type="submit" value="Spara product" class="btn btn-success" style="float:right;"><p></p>
-    </form>
-
-    </div>
-    <div class="col-md-2">
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-8">
 
       @foreach($reviews as $review)
         <div class="panel panel-default">
@@ -79,5 +47,6 @@
     <div class="col-md-2">
     </div>
     </div>
-
+    <br>
+    <br>
 @endsection
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -7,53 +7,72 @@
   <div class="col-md-1">
   </div>
   <div class="col-md-10">
-    <br>
-    <br>
-    <h2 class="head">Mobiler</h2>
-  
-      <div class="media">
-      </div>
-      <div class="media">
-      </div>
-      <div class="media">
-      </div>
-  </div>
-  </div>
-  <div class="col-md-1">
-  </div>
+    <h2>Välkommen till Media.se</h2>
+<p>Vi visar de nyast datorerna, mobilerna och surfplattorna till de bästa priserna.</p>
+<p>Se även i vilka affärer du kan köpa dem.</p>
 </div>
-
+<div class="col-md-1">
+</div>
+<br>
+<br>
 <div class="row">
-  <div class="col-md-1">
-  </div>
-  <div class="col-md-10">
-    <h2 class="head">Surfplattor</h2>
-    <div class="media">
+</div class="col-md-1">
+<div class="col-md-10">
+
+<div class="container" style="width: 500px;">
+  <h4>Nya album</h4>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+
+    <div class="item active">
+      <img src="https://www.o2.co.uk/shop/homepage/images/shop15/brand/apple/iphone-5s/iphone5s-gallery-img-2.jpg" alt="Iphone 5s" style="width:100%;">
+      <div class="carousel-caption">
+        <h3>Iphone 5s</h3>
+        <p>2999</p>
+      </div>
     </div>
-  </div>
-  <div class="col-md-1">
-  </div>
-</div>
 
-<div class="row">
-  <div class="col-md-1">
-  </div>
-  <div class="col-md-10">
-    <h2 class="head">Datorer</h2>
-    <div class="media">
+    <div class="item">
+      <img src="https://www.att.com/catalog/en/skus/images/samsung-galaxy%20s7-black%20onyx-450x350.png" alt="Galaxy s7" style="width:100%;">
+      <div class="carousel-caption">
+        <h3>Galaxy s7</h3>
+        <p>Kristaller</p>
+      </div>
     </div>
+
+    <div class="item">
+      <img src="https://cdn.macrumors.com/article-new/2016/10/macbook-pro-late-2016-800x701.jpg" alt="MacBook Pro" style="width:100%;">
+      <div class="carousel-caption">
+        <h3>MacBook Pro</h3>
+        <p></p>
+
   </div>
-  <div class="col-md-1">
   </div>
 </div>
-
-<div class="row">
-  <div class="col-md-1">
-  </div>
-  <div class="col-md-10">
-    <br>
-    <br>
-    <br>
+</div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+<br>
+<br>
+<br>
   </div>
   <div class="col-md-1">
   </div>
